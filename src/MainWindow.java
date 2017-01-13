d4227bc78be34cc7be2de3a8468db6e45d22f8e7
TheWinged7 <thewinged7@hotmail.com>
2017-01-13 13:10:56 +1100

    added trayIcon, fixed deletion, fixed checkTitle
    + added relevant imports for system tray
    + moved each tasks individual timer into an array of timers so we can
    keep them correct while adding/deleting
    + updated checkTitle to properly check if there is a task by the same
    name already.
    + added a way to update all timers timerActionListeners to ahve the
    correct IDs whenever there is a deletion
    +updated start/stop buttons to use the array instead of the static
    timers so that deletion works properly
    +added taskTray that displays the currently focused timer and its time
    on right-click

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -2,6 +2,8 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
+import java.awt.Image;
+import java.awt.SystemTray;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
@@ -19,6 +21,7 @@ public class MainWindow {
 	private static Gson gson = new Gson();
 	private static ArrayList<Task>  tabs = new ArrayList<Task>();
 	private static ArrayList<JLabel>  tabTimeLabels = new ArrayList<JLabel>();
+	private static ArrayList<Timer> tabTimers = new ArrayList<Timer>();
 	
 	
 	
@@ -27,43 +30,55 @@ public class MainWindow {
 	 */
 	
 	
-	
+	//check the title input by user to make sure its valid
 	private static boolean checkTitle(String title)
 	{
+		//if title already exists, display error popup and return false
+		for (int i=0; i<tabs.size(); i++)
+		{
+			if (title.equals( tabs.get(i).getTitle() ) )
+			{
+				JOptionPane.showMessageDialog(null, "ERROR:\nTask with that title already exists", "Title Error!", JOptionPane.WARNING_MESSAGE);
+				return false;
+			}
+		}
 		
-		
-		if (title == null  )
+		if (title == null  ) //if nothing entered, return false and do nothing
 		{
 			return false;
 		}
-		else if(title.length()<=0){
+		else if(title.length()<=0) //else if somehow not null but length is 0, display error popup and return false
+		{
 			JOptionPane.showMessageDialog(null, "ERROR:\nEmpty titles are not allowed", "Title Error!", JOptionPane.WARNING_MESSAGE);
 			return false;
 		}
-		else if (!Pattern.matches(".*[\\w].*", title))
+		else if (!Pattern.matches(".*[\\w].*", title)) //if only non-standard characters, display error popup and return false
 		{
 			JOptionPane.showMessageDialog(null, "ERROR:\nTitle must contain at least one character other than space", "Title Error!", JOptionPane.WARNING_MESSAGE);
 			return false;
 		}
-		else if(tabs.contains(title))
+		/*
+		else if(alreadyExists) //if the title already exists exactly, display error popup and return false
 		{
 			JOptionPane.showMessageDialog(null, "ERROR:\nTask with that title already exists", "Title Error!", JOptionPane.WARNING_MESSAGE);
 			return false;
-		}
-		else
+		}*/
+		else //if none of the above are true, return true
 		{
 			return true;
 		}
 	}
 	
-	static private class timerActionListener implements ActionListener {
-		int taskID;
+	//action listener for timers that start ticking when that timer is running
+	private static class timerActionListener implements ActionListener {
+		int taskID;  //is used to match the actionListener to the task and timer label so only the correct running one is ticked
 		
 		public timerActionListener(int taskNo)
 		{
 			taskID = taskNo;
 		}
 		
+
 		public void actionPerformed (ActionEvent e)
 		{
 			tabs.get(taskID).tick();
@@ -73,53 +88,83 @@ public class MainWindow {
 
 	}
 	
+	//update current timerActionListener taskIDs so that they can continue to work after one has been deleted
+	private static void updateTimerIDs ()
+	{
+		//Iterate through all timers
+		for (int i=0; i< tabTimers.size(); i++)
+		{
+			ActionListener [] t = tabTimers.get(i).getActionListeners() ;  //get each ones list of action listeners
+			boolean wasRunning= tabTimers.get(i).isRunning();  //check if the current task is running
+			if (wasRunning)
+				{
+				tabTimers.get(i).stop(); //if it was, pause the timer while we update it
+				}			
+			//iterate through all action listeners for the task (should only be 1:1, but better to be safe)
+			for (int j=0; j<t.length; j++)
+			{
+				 tabTimers.get(i).removeActionListener( t[0]) ; //delete each one, which as we delete one, the others become the first
+				 												//meaning we want to delete at element 0 every time
+			}
+			 
+			tabTimers.get(i).addActionListener(new timerActionListener(i)); //give the task a new actionListener with its now correct taskID
+			
+			if (wasRunning)
+			{
+				tabTimers.get(i).start(); //if the task was running when we started, start it running again
+			}
+		}
+	}
+	
+	//add a new task in its own tab
 	private static void addTab(final JTabbedPane tabbedPane, final String label, int h, int m, int s) {
-		
-		
-			final Timer timer = new Timer (100, new timerActionListener(tabs.size()) );
-		 	tabs.add(new Task(label, h, m, s));
-		 	JPanel contentPanel = new JPanel();
-		 	contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
-		 	Box newContentBox = Box.createVerticalBox();
-			contentPanel.add(newContentBox);
+					
+			
+			tabTimers.add(new Timer (100, new timerActionListener(tabs.size()) )); 	//add a new timer with its own timerActionListener to tabTimers
+		 	tabs.add(new Task(label, h, m, s));										//create a new task with the specified label and time to tabs
+		 	JPanel contentPanel = new JPanel();										//create a new JPanel to hold this task
+		 	contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));	//set its layout to boxLayout
+		 	Box newContentBox = Box.createVerticalBox();							//create a new verticalBox to hold the main elements
+			contentPanel.add(newContentBox);										//and add the verticalBox to the JPanel
 		 	
 
 						
-			Box taskNameBox = Box.createHorizontalBox();
-			newContentBox.add(taskNameBox);
+			Box taskNameBox = Box.createHorizontalBox();							//create a horizontalBox to hold just the task name label
+			newContentBox.add(taskNameBox);											//and add it to the verticalBox
 			
-			Component horizontalStrut_5 = Box.createHorizontalStrut(20);
+			Component horizontalStrut_5 = Box.createHorizontalStrut(20);			//create and add spacing to the horizontalBox
 			taskNameBox.add(horizontalStrut_5);
 			
-			JLabel taskLabel = new JLabel(label);
+			JLabel taskLabel = new JLabel(label);									//create JLabel for task name and add it to the taskNameBox
 			taskNameBox.add(taskLabel);
-			taskLabel.setFont(new Font("Courier New", Font.BOLD, 20));
+			taskLabel.setFont(new Font("Courier New", Font.BOLD, 20));				//set its font
 			
-			Component horizontalStrut = Box.createHorizontalStrut(20);
+			Component horizontalStrut = Box.createHorizontalStrut(20);				//create and add spacing to the other side of the JLabel
 			taskNameBox.add(horizontalStrut);
 			
-			JButton taskCreateButton = new JButton("Delete Task");
-			taskCreateButton.addMouseListener(new MouseAdapter() {
+			JButton taskCreateButton = new JButton("Delete Task");  				//create a button that is for deleting the current task
+			taskCreateButton.addMouseListener(new MouseAdapter() {					//add the buttons action listener
 				@Override
 				public void mouseClicked(MouseEvent e) {
 										
-					for (int i=0; i< tabs.size(); i++)
-					{
-						
-						if (tabs.get(i).getTitle() == label )
-						{
-							tabs.remove(i);
+					for (int i=0; i< tabs.size(); i++) 								//iterate through all the tabs
+					{	
+						if (tabs.get(i).getTitle() == label ) 						//and if its label matches this ones
+						{															//delete it from tabs, tabTimeLabels, tabTimers
+							tabs.remove(i);											//and remove it from the tabBox too
 							tabTimeLabels.remove(i);
+							tabTimers.remove(i);
 							taskTabs.removeTabAt(
 									taskTabs.getSelectedIndex() );
-							
-							System.out.println("Found it!");
+
+							updateTimerIDs();										//and then update all taskTimers
 							break;
 						}
 					}
+					 
 				}
 			});
-			taskNameBox.add(taskCreateButton);
+			taskNameBox.add(taskCreateButton);										//add the delete button to the taskNameBox
 			
 			Component horizontalGlue_1 = Box.createHorizontalGlue();
 			taskNameBox.add(horizontalGlue_1);
@@ -146,13 +191,28 @@ public class MainWindow {
 
 					if (timerToggleButton.isSelected())
 					{
-						timer.start();
-						System.out.println("Starting Timer...");
+						for (int i=0; i< tabs.size(); i++)
+						{
+							
+							if (tabs.get(i).getTitle() == label )
+							{
+								tabTimers.get(i).start();
+								System.out.println("Starting Timer...");
+							}
+						}
+					
 					}
 					else 
 					{
-						timer.stop();
-						System.out.println("Stopping Timer...");
+						for (int i=0; i< tabs.size(); i++)
+						{
+							
+							if (tabs.get(i).getTitle() == label )
+							{
+								tabTimers.get(i).stop();
+								System.out.println("Stopping Timer...");
+							}
+						}
 					}
 					
 				}
@@ -256,10 +316,12 @@ public class MainWindow {
 	private void initialize() {
 		
 		
+		
 		CloseListener cl = new CloseListener("Are you sure you want to exit the application?",
 			    "Exit Application");
 		
 		frmTasksTimer = new JFrame();
+		frmTasksTimer.setIconImage(Toolkit.getDefaultToolkit().getImage(MainWindow.class.getResource("/icons/timer_transparent.png")));
 		frmTasksTimer.setResizable(false);
 		frmTasksTimer.getContentPane().setBackground(SystemColor.windowBorder);
 		frmTasksTimer.setTitle("Tasks Timer");
@@ -323,6 +385,68 @@ public class MainWindow {
 		
 		
 		
+		
+		final PopupMenu trayMenu = new PopupMenu();
+		Image icon = Toolkit.getDefaultToolkit().getImage( MainWindow.class.getResource("/icons/timer.png"));
+        final TrayIcon trayIcon = new TrayIcon( icon, 
+        				"Task Timer" );  //add in image same as icon later
+		
+		
+		final MenuItem currentTask = new MenuItem(taskTabs.getTitleAt( taskTabs.getSelectedIndex()));
+		final MenuItem newTask = new MenuItem("New Task");
+		
+		currentTask.setEnabled(false);
+		newTask.addActionListener(  new ActionListener() {
+			
+			public void actionPerformed (ActionEvent e) {
+				String title = (String) JOptionPane.showInputDialog("Please input task name", null);
+				if (checkTitle(title))
+					{
+						addTab(taskTabs, title, 0, 0, 0);
+					}
+			
+				}
+			}
+				
+				);
+		
+
+		trayMenu.add(newTask);
+		
+		
+		trayIcon.setPopupMenu(trayMenu);
+		trayIcon.setImageAutoSize(true);
+		trayIcon.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				if (frmTasksTimer.getState() == java.awt.Frame.NORMAL)
+				{
+					frmTasksTimer.toBack();
+					frmTasksTimer.setState(java.awt.Frame.ICONIFIED);
+				}
+				else if (frmTasksTimer.getState() == java.awt.Frame.ICONIFIED)
+				{
+					frmTasksTimer.toFront();
+					frmTasksTimer.repaint();
+					frmTasksTimer.setState(java.awt.Frame.NORMAL);
+				}
+				
+
+			}
+		});
+		
+		if (SystemTray.isSupported())
+		{
+			final SystemTray tray = SystemTray.getSystemTray();
+			try {
+				tray.add(trayIcon);
+			} 
+		catch (AWTException e) {
+            	System.out.println("TrayIcon could not be added.");
+        	}
+		}
+		
 		taskTabs.addChangeListener(new ChangeListener() {
 			
 			public void stateChanged (ChangeEvent e){
@@ -336,6 +460,10 @@ public class MainWindow {
 						
 					}
 
+				}
+				else
+				{
+					currentTask.setLabel(taskTabs.getTitleAt( taskTabs.getSelectedIndex()));
 				}
 			}
 			
@@ -372,6 +500,11 @@ public class MainWindow {
 	        }
 	    }));
 	
+		
+		
+		
+		
+		
 		
 	}
 
